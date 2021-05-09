b817caad1106ab49534c5f5f8ca5e0097f72c76a
暗号 <626522796@qq.com>
2021-05-09 08:56:08 +0800

    plot trendline

--- fans_count.py
+++ fans_count.py
@@ -4,9 +4,18 @@ data = urllib.request.urlopen("https://api.bilibili.com/x/web-interface/card?mid
 xinxi = data.read().decode('utf-8')
 dict = json.loads(xinxi)
 fans = dict['data']['card']['fans']
-t = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
+t = time.strftime('%Y-%m-%d-%H-%M', time.localtime())
 
-add_info = '\n' + t + ' ' + str(fans) + '\n'
+add_info = t + ',' + str(fans) + '\n'
 
-with open('README.md','a') as f:
+with open('data.csv','a') as f:
     f.write(add_info)
+    
+
+from pandas import read_csv
+import matplotlib.pyplot as plt
+
+series = read_csv('data.csv',header=0, index_col=0, parse_dates=None, squeeze=True)
+
+series.plot()
+plt.show()
